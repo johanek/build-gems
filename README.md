92b26e4960e0039cf7cdb5db32adabe499d0e11d
Johan van den Dorpe <jvd@dneg.com>
2012-04-27 09:59:12 +0100

    Imported scripts

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 build-gems
 ==========
+
+Builds all gems in gem-list
+
+- Based on https://github.com/KrisBuytaert/build-gems 
+- Builds all dependencies as individual rpms
+- Works around fpm bug where you can't specify gem version (Github fpm bug #204)
+- Doesn't bother rebuilding gems in gem-list which have been built before
---
+++ build-gems.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+FPM=/usr/bin/fpm
+
+if [ ! -d BUILD ]
+then
+  mkdir -p BUILD
+fi
+
+for gem in `cat gem-list | grep -v "#" `
+do
+  version=`echo $gem | awk -F':' '{print $2}'`
+  basename=`echo $gem | awk -F':' '{print $1}'`
+  cd BUILD
+  if [ x$version == 'x' ]; then
+    version=`gem list -r $basename | grep "^$basename " | sed "s/.*(\([0-9.]*\).*/\1/"`
+  fi
+
+  gembuilddir="gembuild/${basename}/${version}"
+  donefile="${gembuilddir}/done"
+
+  if [ -e $donefile ]; then
+    echo "Already exists, skipping: $basename-$version"
+    RETVAL=0
+  else
+    if [ ! -d $gembuilddir ]; then
+      mkdir -p $gembuilddir
+    fi
+
+    echo "Finding dependencies for $basename-$version"
+    echo 
+
+    gem install --no-ri --no-rdoc --install-dir $gembuilddir -v $version $basename
+
+    GEMRETVAL=$?
+    echo "Gem install returned with $GEMRETVAL"
+    [ $GEMRETVAL -ne 0 ] && exit 1
+
+    for g in `ls -1 ${gembuilddir}/cache`
+    do
+      echo "Building ${g}"
+      echo 
+      ${FPM} -s gem -t rpm ${gembuilddir}/cache/${g}
+      RETVAL=$?
+    done
+  
+  	echo "Build of $basename returned with $RETVAL"
+  	echo 
+  	[ $RETVAL -ne 0 ] && exit 1
+	fi
+  
+  touch $donefile  
+  cd ../
+
+done
